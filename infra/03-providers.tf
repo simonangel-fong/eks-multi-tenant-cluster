93b4ee0541f361bfd5df92d992cf3a2eb3286dae
simonangel-fong <simonangelfong@gmail.com>
2026-07-08 15:15:28 -0400

    argocd: debug prom grafana

--- infra/03-providers.tf
+++ infra/03-providers.tf
@@ -24,6 +24,10 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
   backend "s3" {}
 }
